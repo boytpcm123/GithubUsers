360d1af34581b9da0340f211c7216920824f3a73
thong.nguyen <nguyenthuongthongcm@gmail.com>
2024-07-19 22:53:01 +0700

    Init base project

---
+++ GithubUsers/GithubUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubUsersApp.swift
+//  GithubUsers
+//
+//  Created by Thong Nguyen on 19/7/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
